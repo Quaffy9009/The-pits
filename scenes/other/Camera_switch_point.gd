9df7a7c5c402db0008711b4881e5b4ac7a519380
SleepingPhoenix1 <SleepingPhoenix2@gmail.com>
2022-02-08 19:11:39 +0200

    added camera switching and movement
    finally!

--- scenes/other/Camera_switch_point.gd
+++ scenes/other/Camera_switch_point.gd
@@ -4,19 +4,38 @@ export (NodePath) var current_camera
 export (NodePath) var switch_to_left
 export (NodePath) var switch_to_right
 export (NodePath) var player
+
+
 export var one_time_pass = false
 
 
-
 func _on_Area2D_body_entered(body):
-	if switch_to_left:
+	pass
+	
+
+
+func _on_Area2D_body_exited(body):
+	if one_time_pass and body.is_in_group("player"):
+		$StaticBody2D/CollisionShape2D.set_deferred("disabled", false)
+		print("S")
+	if switch_to_left and current_camera == switch_to_right:
 		if body.is_in_group("player"):
 			get_node(switch_to_left).current = true
+			#get_node(current_camera).current = false
 			current_camera = switch_to_left
+			get_node(player).get_node("RemoteTransform2D").set_remote_node(switch_to_left)
 	
-	if switch_to_right:
+	elif switch_to_right and current_camera == switch_to_left:
 		if body.is_in_group("player"):
+			#get_node(current_camera).current = false
 			get_node(switch_to_right).current = true
 			current_camera = switch_to_right
-	if one_time_pass:
-		$StaticBody2D/CollisionShape2D2.disabled = false
+			get_node(player).get_node("RemoteTransform2D").set_remote_node(switch_to_right)
+
+func _process(delta):
+	get_node(switch_to_left).limit_right = $camera_limit_right.global_position.x
+	get_node(switch_to_right).limit_left = $camera_limit_left.global_position.x
+
+
+
+
