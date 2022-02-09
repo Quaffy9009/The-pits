b2a3cd14ea3e0cc2b9e9b9756f97df571a7f3c6f
SleepingPhoenix1 <SleepingPhoenix2@gmail.com>
2022-02-09 20:58:10 +0200

    basic enemy fixes
    fixed enemy and shell collision

--- scenes/main/enemys/ground/GroundEnemy1.gd
+++ scenes/main/enemys/ground/GroundEnemy1.gd
@@ -12,30 +12,34 @@ func _ready():
 	randomize()
 	if direction == 1:
 		$AnimatedSprite.scale.x = -3
-	$FloorChecker.position.x = $CollisionPolygon2D.shape.get_extents().x * direction
+	$FloorChecker.position.x = 20 * direction
 func _physics_process(delta):
 	if is_on_wall() or not $FloorChecker.is_colliding() and is_on_floor():
-		direction = direction * -1
+		direction = -direction 
 		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
-		$FloorChecker.position.x = $CollisionPolygon2D.shape.get_extents().x * direction
+		$FloorChecker.position.x = 20 * direction
 	velocity.y += 20
 	velocity.x = 15 * direction
 	velocity = move_and_slide(velocity,Vector2.UP)
+
 func _on_PlayerDetector_body_entered(body):
 	Global.ge1_damage = true
-func _on_BulletDetector_area_entered(area):
-	
+
+
+func _on_PlayerDetector_area_entered(area):
 	Global.play_enemy_hit_sound = true
 	if health > 0:
 		health = health - 1
-	elif health <= 0:
+	else:
 		die()
+
+
 func die():
 	var shell_drop_amount = (randi() % 5) 
 	while shell_drop_amount > 0:
 		var new_shell = SHELL_SCENE.instance()
 		new_shell.position = shelldrop.global_position
-		get_tree().current_scene.add_child(new_shell)
+		get_parent().call_deferred("add_child", new_shell)
 		shell_drop_amount -= 1
 	if shell_drop_amount == 0:
 		queue_free()
