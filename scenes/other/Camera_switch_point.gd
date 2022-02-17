e786172e6ce619124d4ba6d77e5ed69ed35320ed
SleepingPhoenix1 <SleepingPhoenix2@gmail.com>
2022-02-17 18:28:55 +0200

    fixes

--- scenes/other/Camera_switch_point.gd
+++ scenes/other/Camera_switch_point.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-export var enabled = true
+
 export (NodePath) var current_camera
 export (NodePath) var player
 export (NodePath) var next_point
@@ -17,39 +17,30 @@ export var one_time_pass = false
 
 
 func _on_Area2D_body_entered(body):
-	if enabled:
-		if one_time_pass and body.is_in_group("player"):
+	if body.is_in_group("player"):
+		if one_time_pass:
 			$StaticBody2D/CollisionShape2D.set_deferred("disabled", false)
 			
 		if camera_left_exist and current_camera_right:
-			if body.is_in_group("player"):
-				#get_node(switch_to_left).current = true
-				#current_camera = switch_to_left
-				#get_node(player).get_node("RemoteTransform2D").set_remote_node(switch_to_left)
-				current_camera_left = true
-				current_camera_right = false
-				if vertical:
-					get_node(current_camera).limit_right = $camera_limit.global_position.x
-					if prev_point and get_node(prev_point).vertical:
-						get_node(current_camera).limit_left = get_node(prev_point).get_node("camera_limit").global_position.x
-					else: get_node(current_camera).limit_left = -10000000
-				$StaticBodyRight/CollisionShape2D.set_deferred("disabled", false)
+			current_camera_left = true
+			current_camera_right = false
+			if vertical:
+				get_node(current_camera).limit_right = $camera_limit.global_position.x
+				if prev_point and get_node(prev_point).vertical:
+					get_node(current_camera).limit_left = get_node(prev_point).get_node("camera_limit").global_position.x
+				else: get_node(current_camera).limit_left = -10000000
+			$StaticBodyRight/CollisionShape2D.set_deferred("disabled", false)
 		
 		elif camera_right_exist and current_camera_left:
-			if body.is_in_group("player"):
-				#print(get_node(next_point).vertical)
-				#get_node(switch_to_right).current = true
-				#current_camera = switch_to_right
-				#get_node(player).get_node("RemoteTransform2D").set_remote_node(switch_to_right)
-				current_camera_left = false
-				current_camera_right = true
-				if vertical:
-					get_node(current_camera).limit_left = $camera_limit.global_position.x
-					if next_point and get_node(next_point).vertical:
-						get_node(current_camera).limit_right = get_node(next_point).get_node("camera_limit").global_position.x
-						
-					else: get_node(current_camera).limit_right = 1000000
-				$StaticBodyLeft/CollisionShape2D.set_deferred("disabled", false)
+			current_camera_left = false
+			current_camera_right = true
+			if vertical:
+				get_node(current_camera).limit_left = $camera_limit.global_position.x
+				if next_point and get_node(next_point).vertical:
+					get_node(current_camera).limit_right = get_node(next_point).get_node("camera_limit").global_position.x
+					
+				else: get_node(current_camera).limit_right = 1000000
+			$StaticBodyLeft/CollisionShape2D.set_deferred("disabled", false)
 	
 
 
